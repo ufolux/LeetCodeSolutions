ce780a277cddc83b438ee007193c1cb868d4e76a
Sines <richadlu001@gmail.com>
2020-02-16 16:25:54 +0800

    L188

--- src/L188_BestTimetoBuyAndSellStockIV/Solution.java
+++ src/L188_BestTimetoBuyAndSellStockIV/Solution.java
@@ -1,19 +1,35 @@
 package L188_BestTimetoBuyAndSellStockIV;
 
+import java.util.Arrays;
+
 public class Solution {
     public int maxProfit(int k, int[] prices) {
-        int lastSell = 0, lastBuy = Integer.MIN_VALUE;
+        if (prices == null || prices.length < 2 || k < 1) {
+            return 0;
+        }
+
+        if (k > prices.length / 2) {
+            int max = 0;
+            for (int i = 1; i < prices.length; i++) {
+                if (prices[i] > prices[i - 1]) {
+                    max += prices[i] - prices[i - 1];
+                }
+            }
+            return max;
+        }
+
+        int[] buys = new int[k], sells = new int[k];
+        Arrays.fill(buys, Integer.MIN_VALUE);
         for (int i = 0; i < prices.length; i++) {
             int p = prices[i];
-            for (int j = 0; j < k; j++) {
-                if (j == 0) lastBuy = Math.max(lastBuy, -p);
-                else {
-                    lastBuy = Math.max(lastBuy, lastSell - p);
-                }
-                lastSell = Math.max(lastSell, lastBuy + p);
+            buys[0] = Math.max(buys[0], -p);
+            sells[0] = Math.max(sells[0], buys[0] + p);
+            for (int j = 1; j < k; j++) {
+                buys[j] = Math.max(buys[j], sells[j - 1] - p);
+                sells[j] = Math.max(sells[j], buys[j] + p);
             }
         }
-        return lastSell;
+        return sells[sells.length - 1];
     }
 
     public static void main(String[] args) {
