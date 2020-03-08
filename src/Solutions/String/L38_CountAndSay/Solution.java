196251bf3655573bb873214f410a50f6a5bb33f6
Xin Lu <xin@MacBook-Pro.local>
2020-03-08 13:05:44 +0800

    Unfinished

---
+++ src/Solutions/String/L38_CountAndSay/Solution.java
@@ -0,0 +1,15 @@
+package Solutions.String.L38_CountAndSay;
+
+import java.util.*;
+
+public class Solution {
+    public String countAndSay(int n) {
+        List a = new LinkedList();
+        return "";
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        s.countAndSay(5);
+    }
+}
