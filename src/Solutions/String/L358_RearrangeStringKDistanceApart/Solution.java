56413315fc7639129df256843ed027af4338510a
Xin Lu <xin@MacBook-Pro.local>
2020-03-11 11:23:24 +0800

    L358 explaination

--- src/Solutions/String/L358_RearrangeStringKDistanceApart/Solution.java
+++ src/Solutions/String/L358_RearrangeStringKDistanceApart/Solution.java
@@ -6,6 +6,11 @@ public class Solution {
     /**
      * 1. use map to cal letter frequency
      * 2. use heap (PQ) to get highest freq letter every time
+     *
+     * theory: we need to put letters according to their frequency, from high to low.
+     * So, the priority queue provide a heap implementation for us to get the highest frequency letter each time.
+     * We could get all k highest frequency letters from the heap, then arrange them into a char sequence and append it to the StringBuilder object.
+     * After that, re-add these k letters into the heap with frequency number minus 1, if their frequency is still greater than 0
      * @param s
      * @param k
      * @return
