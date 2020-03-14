d163c1792ea614575e74c9b278e5222e877cc11d
ufolux <richadlu001@gmail.com>
2020-03-14 20:10:27 +0800

    [L12]

---
+++ src/Solutions/String/L12_IntegerToRoman/Solution.java
@@ -0,0 +1,22 @@
+package Solutions.String.L12_IntegerToRoman;
+
+public class Solution {
+    public String intToRoman(int num) {
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] romans = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < values.length; i++) {
+            while (num >= values[i]) {
+                num -= values[i];
+                sb.append(romans[i]);
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.intToRoman(1233));
+    }
+}
