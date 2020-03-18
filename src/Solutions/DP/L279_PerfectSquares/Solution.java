e8be2a177a83ae65438e524e39532b4acdf3cf80
ufolux <richadlu001@gmail.com>
2020-03-18 23:18:56 +0800

    L165

--- src/Solutions/DP/L279_PerfectSquares/Solution.java
+++ src/Solutions/DP/L279_PerfectSquares/Solution.java
@@ -7,7 +7,7 @@ class Solution {
     public int numSquares(int n) {
         int[] dp = new int[n + 1];
         for (int i = 1; i <= n; i++) {
-            dp[i] = i;
+            dp[i] = i; // 最坏的情况就是每次+1
             for (int j = 1; i - j*j >= 0; j++) {
                 // 每个完美平方数都是由一个完全平方数和一个普通数字组成
                 // +1 指的是一个完全平方数
@@ -19,6 +19,7 @@ class Solution {
 
     public static void main(String[] args) {
         Solution s = new Solution();
+        Integer a = new Integer("07");
         s.numSquares(12);
     }
 }
