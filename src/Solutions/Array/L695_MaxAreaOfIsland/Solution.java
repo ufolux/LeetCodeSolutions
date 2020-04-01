5eb2191e2f3ef6734a70b5303001a996ff7d0c06
Sines <richadlu001@gmail.com>
2020-04-01 16:09:23 +0800

    L695 revise

---
+++ src/Solutions/Array/L695_MaxAreaOfIsland/Solution.java
@@ -0,0 +1,39 @@
+package Solutions.Array.L695_MaxAreaOfIsland;
+
+public class Solution {
+    public int maxAreaOfIsland(int[][] grid) {
+        if (grid == null || grid.length == 0) return 0;
+        int res = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                res = Math.max(res, dfs(i, j, grid));
+            }
+        }
+        return res;
+    }
+
+    int dfs(int i, int j, int[][] grid) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == 0) return 0;
+        grid[i][j] = 0;
+        int num = 1;
+        num += dfs(i - 1, j, grid);
+        num += dfs(i + 1, j, grid);
+        num += dfs(i, j - 1, grid);
+        num += dfs(i, j + 1, grid);
+        return num;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.maxAreaOfIsland(new int[][]{
+                {0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+                {0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0},
+                {0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0}
+        }));
+    }
+}
