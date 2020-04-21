2a1c7bc656fff8fa528effe58266012934dfd54c
Sines <richadlu001@gmail.com>
2020-04-21 17:30:23 +0800

    Backtracking

--- src/BasicAlgos/permutation/GenPermutationSeq.java
+++ src/BasicAlgos/permutation/GenPermutationSeq.java
@@ -1,33 +1,72 @@
 package BasicAlgos.permutation;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class GenPermutationSeq {
     /**
-     * @param src
+     * 递归法全排列
+     * Non Lexicographical Order
+     * @param nums
      * @return
      */
-    public List<List<Integer>> genSeq1 (int[] src) {
-        for (int i = 0; i < src.length; i++) {
-
-        }
-        return null;
+    public List<List<Integer>> permute1(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        genPermute1(res, nums, 0);
+        return res;
     }
 
-    void permutateion (List<List<Integer>> res, int[] src, int start) {
-        if (start != src.length - 1) {
-            Arrays.asList(null);
+    public void genPermute1(List<List<Integer>> res, int[] nums, int start) {
+        if (start == nums.length - 1) {
+            List<Integer> l = Arrays.stream(nums).boxed().collect(Collectors.toList());
+            res.add(l);
+            return;
+        }
+        for (int i = start; i < nums.length; i++) {
+            swap(nums, start, i); // swap
+            genPermute1(res, nums, start + 1); // permute start + 1 to arr.length;
+            swap(nums, start, i); // recovery
+        }
+    }
+
+    public void swap(int[] nums, int i, int j) {
+        int t = nums[i];
+        nums[i] = nums[j];
+        nums[j] = t;
+    }
+
+    /**
+     * 回溯法
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> permute2(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        int[] visited = new int[nums.length];
+        backTrack(res, nums, new ArrayList<>(), visited);
+        return res;
+    }
+
+    public void backTrack (List<List<Integer>> res, int[] nums, List<Integer> tmp, int[] visited) {
+        if (tmp.size() == nums.length) {
+            res.add(new ArrayList<>(tmp));
+            return;
         }
 
+        for (int i = 0; i < nums.length; i++) {
+            if (visited[i] == 1) continue;
+            visited[i] = 1;
+            tmp.add(nums[i]);
+            backTrack(res, nums, tmp, visited);
+            visited[i] = 0;
+            tmp.remove(tmp.size() - 1);
+        }
     }
+
 
     public static void main(String[] args) {
-        int[] a = new int[12];
-        Arrays.asList(null);
         GenPermutationSeq s = new GenPermutationSeq();
-        var res = s.genSeq1(new int[]{1,2,3,4,5});
+        s.permute1(new int[]{1,2,3,4,5});
+        s.permute2(new int[]{1,2,3,4,5});
     }
 }
---
+++ src/Solutions/Backtracking/Solution.java
@@ -0,0 +1,31 @@
+package Solutions.Backtracking;
+
+import java.util.*;
+
+public class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        int[] visited = new int[nums.length];
+        backtrack(res, nums, visited, new ArrayList<>());
+        return res;
+    }
+
+    public void backtrack(List<List<Integer>> res, int[] nums, int[] visited, List<Integer> tmp) {
+        if (tmp.size() == nums.length) {
+            res.add(new ArrayList<>(tmp));
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (visited[i] == 1) continue;
+            visited[i] = 1;
+            tmp.add(nums[i]);
+            backtrack(res, nums, visited, tmp);
+            visited[i] = 0;
+            tmp.remove(tmp.size() - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        s.permute(new int[]{1, 2, 3, 4, 5});
+    }
+}
