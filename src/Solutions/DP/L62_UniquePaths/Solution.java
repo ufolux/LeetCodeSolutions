75e9bfdc71f01dfed8cf48144d5840442bc943f5
ufolux <richadlu001@gmail.com>
2021-01-19 22:57:04 +0800

    fix

--- src/Solutions/DP/L62_UniquePaths/Solution.java
+++ src/Solutions/DP/L62_UniquePaths/Solution.java
@@ -11,9 +11,6 @@ public class Solution {
         for (int i = 0; i < n; ++i) {
             dp[0][i] = 1;
         }
-
-
-        dp[1][0] = 1;
         for (int i = 1; i < m; ++i) {
             for (int j = 1; j < n; ++j) {
                 dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
