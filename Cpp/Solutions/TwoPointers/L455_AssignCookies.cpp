e14f6390f8b72b8051a33965d9d2a2936c126599
ufolux <richadlu001@gmail.com>
2021-02-03 20:52:17 +0800

    Add Solution

---
+++ Cpp/Solutions/TwoPointers/L455_AssignCookies.cpp
@@ -0,0 +1,32 @@
+//
+// Created by Sines on 2021/2/3.
+//
+
+#include <Solutions/BaseSolution.h>
+#include "PublicHeaders.h"
+
+namespace L455_AssignCookies {
+    using namespace std;
+    class Solution: BaseSolution {
+    public:
+        int findContentChildren(vector<int> &g, vector<int> &s) {
+            sort(g.begin(), g.end());
+            sort(s.begin(), s.end());
+            int glen = g.size();
+            int slen = s.size();
+            int ans = 0;
+            for (int i = 0, j = 0; i < glen && j < slen;) {
+                if (g[i] <= s[j]) {
+                    ans++;
+                    i++;
+                    j++;
+                } else {
+                    j++;
+                }
+            }
+            return ans;
+        }
+
+
+    };
+};
